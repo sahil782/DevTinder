94b7b00b11c53bc4815ebadaa210694e86f2b68b
sahil782 <singlasahil.1154@gmail.com>
2025-07-12 22:52:04 +0530

    routes handling

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 const express = require('express');
-
 const app = express();
 
-app.listen('3000',()=> {
-    console.log("Server is listening on port 3000");
+app.use('/hello', (req, res) => {
+  res.send('Test');
+});
+
+app.get('/user/:userId/:userName/:password',(req,res) => {
+    // console.log(req.query);
+    console.log(req.params);
     
+    res.send({firstName : "Sahil", lastName : "Singla"})
 })
 
-app.use("/hello",(req,res)=> {
-    res.send("challl gya mei");
+app.post('/user',(req,res) => {
+    res.send("Stored in database successfully")
 })
+
+app.delete('/user',(req,res) => {
+    res.send("Deleted successfully from databasse");
+})
+
+// app.use('/', (req, res) => {
+//   res.send('HelloWorld');
+// });
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
