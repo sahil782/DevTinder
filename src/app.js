f43dd356a741448d8f14ef0761439739c015589d
sahil782 <singlasahil.1154@gmail.com>
2025-07-13 00:13:17 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.use('/hello', (req, res) => {
-  res.send('Test');
-});
-
-app.get('/user/:userId/:userName/:password',(req,res) => {
-    // console.log(req.query);
-    console.log(req.params);
+app.use("/user",(req,res,next)=>{
+    console.log("Response1");
+     //res.send("Response1");
+    next();
     
-    res.send({firstName : "Sahil", lastName : "Singla"})
+},(req,res,next)=>{
+    console.log("Response 2");
+    //res.send("Response2");
+    next();
+    
+},(req,res,next)=>{
+    console.log("Response 3");
+    //res.send("Response3");
+    next();
+    
+},(req,res,next)=>{
+    console.log("Response 4");
+    // res.send("Response4");
+    next();
+    
+},(req,res,next)=>{
+     console.log("Response 5");
+    res.send("Response5");
+    
 })
-
-app.post('/user',(req,res) => {
-    res.send("Stored in database successfully")
-})
-
-app.delete('/user',(req,res) => {
-    res.send("Deleted successfully from databasse");
-})
-
-// app.use('/', (req, res) => {
-//   res.send('HelloWorld');
-// });
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
