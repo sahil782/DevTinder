ed67005a1671ecb26615a488a151e985e495955e
sahil782 <singlasahil.1154@gmail.com>
2025-07-20 14:28:03 +0530

    dynamic api signup

--- src/app.js
+++ src/app.js
@@ -1,47 +1,35 @@
 const express = require('express');
 const app = express();
-const {adminAuth,userAuth} = require('./middlewares/auth')
-// so Basically before the res.send if request goes to multiple routes then all they act like as a middleware its all middleware chain.
-app.use("/admin",adminAuth)
+const connectDB = require("./config/database");
+const User = require("./models/user")
 
-// you can write middleware even in the route path also
-app.all("/user/specific",userAuth,(req,res,next)=>{
-    console.log("1st attempt");
-    next();
-    
-},(req,res)=>{
-    console.log("2nd attempt");
-    res.send("2nd attempted passed");
-})
 
-app.get("/admin/getData",(req,res)=>{
-    console.log("Sent All Data");
+app.use(express.json());
+app.post("/signup",async (req,res)=>{
+    console.log(req.body);
     
-    res.send("Sent All Data");
-})
-
-app.delete("/admin/delete",(req,res)=>{
-    console.log("Delete All Data");
+    const user = new User(req.body);
+    // console.log(user); return false;
     
-    res.send("Deleted all data");
-})
-// First of all for error handling you have to use try catch
-app.get("/getUserData",(req,res) => {
-    // try{
-        throw new Error("fdada");
-        res.send("User Data Sent")
-    // }catch(err){
-    //     res.status(500).send("some error occured");
-    // }
-    
-});
-// But for globally error we can configure commonly one route
-app.use("/",(err,req,res,next) => {
-    if(err){
-        res.status(500).send("Something went wrong");
+    try{
+        await user.save();
+        res.status(201).send("User saved successfully");
+    }catch(err){
+        res.status(400).send(`Error saving the user ${err.message}`);
     }
 })
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+
+connectDB().then(()=>{
+    console.log("Database connection successfully");
+    app.listen(3000, () => {
+        console.log("Server is listening on port 3000");
+    });
+}).catch((err)=>{
+    console.log("Database cannot be connected");
+    
+})
+
+
+
+
